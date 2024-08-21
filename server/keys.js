6fefa9c2bfa65e422ebe84d9e70c66eeefdf3f62
Kudumula Akshith Yadav <akshithyadav111@gmail.com>
2024-08-21 03:56:04 -0400

    final commit

---
+++ server/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://Murarji:Admin@cluster0.012nw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  JWT_SECRET: "alskdjfhg",
+  DB_user: "Murarji",
+  DB_password: "Admin",
+};
